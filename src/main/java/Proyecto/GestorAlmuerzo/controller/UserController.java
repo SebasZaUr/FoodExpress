869aad87c41aa5933408086c6f5769b397a71f02
Sebastian Zamora <sebassele2008@gmail.com>
2023-11-12 11:44:17 -0500

    prueba

--- src/main/java/Proyecto/GestorAlmuerzo/controller/UserController.java
+++ src/main/java/Proyecto/GestorAlmuerzo/controller/UserController.java
@@ -1,2 +1,15 @@
-package Proyecto.GestorAlmuerzo.controller;public class UserController {
+package Proyecto.GestorAlmuerzo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
 }
